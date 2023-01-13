c4807c13294fff0d1cb460fec78ef8b65eb0ac70
Juan C <juan.s.carrasquilla@gmail.com>
2023-01-13 16:01:41 -0600

    added outline of testing function, created Board and GameLoop classes, moved functions to respective classes, changed match case to dictionary, changed function names to snake case

--- connect_four.py
+++ connect_four.py
@@ -1,144 +1,126 @@
 
-colA, colB, colC, colD, colE, colF, colG = [], [], [], [], [], [], []
-columns = [colA, colB, colC, colD, colE, colF, colG]
+class Board():
+    self.current = [[None]*7 for _ in range(6)]
+    self.colA, self.colB, self.colC, self.colD, self.colE, self.colF, self.colG = [], [], [], [], [], [], []
+    self.columns = [colA, colB, colC, colD, colE, colF, colG]
+    self.col_dict = {"A": [colA, 0], "B": [colB, 1], "C": [colC, 2], "D": [colD, 3], "E": [colE, 4], "F": [colF, 5], "G": [colG, 6]}
 
-board = [[None]*7 for _ in range(6)]
+    def __init__(self):
 
-winner = None
+    #function for printing out the current board and pieces played
+    def print_board(board):
+        row_count = 6
+        for row in board:
+            print_row = str(row_count)
+            
+            for elem in row:
+                if elem is None:
+                    print_row += " _ "
+                else:
+                    print_row += " " + elem + " "
 
-#function for printing out the current board and pieces played
-def printBoard(board):
-    col_count = 6
-    for row in board:
-        print_row = str(col_count)
+            print(print_row)
+            row_count -= 1
         
-        for elem in row:
-            if elem is None:
-                print_row += " _ "
+        print("  A  B  C  D  E  F  G")
+
+
+    #function for checking the validity of a chosen column. Checks if col exists and is not full. 
+    def check_col(col: str): 
+        while True:        
+            if col not in self.col_dict: 
+                print(player1 + ", that column does not exist. Please pick another column (A-G):")
+                col = str(input()).capitalize()
+                continue
+            
+            elif len(self.col_dict[col][0]) >= 6:
+                print(player1 + ", that column is full. Please pick another column (A-G):")
+                col = str(input()).capitalize()
+                continue
             else:
-                print_row += " " + elem + " "
+                return self.col_dict[col]
 
-        print(print_row)
-        col_count -= 1
-    
-    print("  A  B  C  D  E  F  G")
-
-
-#function for checking the validity of a chosen column. Checks if col exists and is not full. 
-def checkCol(col):
-    while True:
-        match col:
-            case "A":
-                col = colA
-                col_index = 0
-            case "B":
-                col = colB
-                col_index = 1
-            case "C":
-                col = colC
-                col_index = 2
-            case "D":
-                col = colD
-                col_index = 3
-            case "E":
-                col = colE
-                col_index = 4
-            case "F":
-                col = colF
-                col_index = 5
-            case "G":
-                col = colG
-                col_index = 6
-            case _:
-                col = None
+    def vertical_win([row, col], color: str):
+        #vertical win
+        if row <= 2:
+            adj_count = 1
+            while adj_count + row <= 5: 
+                if board[row + adj_count][col] == color:
+                    adj_count += 1
+                    if adj_count >= 4:
+                        return True
+                else:
+                    adj_count = 0
         
-        if not col: 
-            print(player1 + ", that column does not exist. Please pick another column (A-G):")
-            col = str(input()).capitalize()
-            continue
-        
-        elif len(col) >= 6:
-            print(player1 + ", that column is full. Please pick another column (A-G):")
-            col = str(input()).capitalize()
-            continue
-        else:
-            return col, col_index
+        return False
 
-
-def checkWin([row, col], color):
-    win = False
-    #vertical win
-    if row <= 2:
-        adj_count = 1
-        while adj_count + row <= 5: 
-            if board[row + adj_count][col] == color:
+    def horizontal_win([row, col], color):
+        #horizontal win
+        adj_count = 0
+        for x in range(7): 
+            if board[row][x] == color:
                 adj_count += 1
                 if adj_count >= 4:
-                    win = True
-                    return win
+                    return True
             else:
-                adj_count = 0
+                    adj_count = 0
+        
+        return False
 
-    #horizontal win
-    adj_count = 0
-    for x in range(7): 
-        if board[row][x] == color:
-            adj_count += 1
-            if adj_count >= 4:
-                win = True
-                return win
-        else:
-            adj_count = 0
+    def diagonal_win([row, col], color):
+        #diagonal win
+        
+        #TODO.........................................
 
-    #diagonal win
-    
-    #TODO.........................................
-
-    return win
+    def check_win([row, col], color):
+        win = False
+        win = vertical_win([row, col], color) or horizontal_win([row, col], color) or diagonal_win([row, col], color)
+        return win
 
 
-printBoard(board)
-# take player's names
-print("Player 1, you are yellow. What is your name?")
-player1 = str(input())
-print("Player 2, you are red. What is your name?")
-player2 = str(input())
 
 # Main gameplay. Set as while loop to play until exited.
-while not winner:
-    
-    #first player's turn
-    print(player1, "it is your turn. Enter the column you would like to add a piece to (A-G):")
-    col_chosen = str(input()).capitalize()
-    valid_col, col_i = checkCol(col_chosen)
-    valid_col.append("Y")
-    new_piece = [6 - len(valid_col), col_i]
+class GameLoop():
+    winner = None
 
-    #add new piece to board and print current board
-    board[new_piece[0]][new_piece[1]] = "Y"
-    printBoard(board)
+    def new_turn(player: str, color: str):
+        print(player, "it is your turn. Enter the column you would like to add a piece to (A-G):")
+        col_chosen = str(input()).capitalize()
+        valid_col, col_i = check_col(col_chosen)
+        valid_col.append(color)
+        new_piece = [6 - len(valid_col), col_i]
 
-    #check if new piece resulted in win
-    if checkWin(new_piece, "Y"):
-        winner = player1
-        print(player1, "you win!! Press R to restart, or X to exit the game.")
-        #TODO.........................................
+        #add new piece to board and print current board
+        board[new_piece[0]][new_piece[1]] = color
+        print_board(board)
 
-    
-    #second player's turn
-    print(player2, "it is your turn. Enter the column you would like to add a piece to (A-G):")
-    col_chosen = str(input()).capitalize()
-    valid_col, col_i = checkCol(col_chosen)
-    valid_col.append("R")
-    new_piece = [6 - len(valid_col), col_i]
+        #check if new piece resulted in win
+        if check_win(new_piece, color):
+            winner = player
+            print(player, "you win!! Press R to restart, or X to exit the game.")
+            #TODO.........................................
 
-    #add new piece to board and print current board
-    board[new_piece[0]][new_piece[1]] = "R"
-    printBoard(board)
 
-    #check if new piece resulted in win
-    if checkWin(new_piece, "R"):
-        winner = player2
-        print(player2, "you win!! Press R to restart, or X to exit the game.")
-        #TODO.........................................
+    # take player's names
+    print("Player 1, you are yellow. What is your name?")
+    player1 = str(input())
+    print("Player 2, you are red. What is your name?")
+    player2 = str(input())
 
+    while not winner:
+        #first player's turn
+        new_turn(player1, "Y")
+        
+        #second player's turn
+        new_turn(player2, "R")
+
+
+def tests():
+    #TODO add tests ..............................
+    #check if board is printed correctly
+    #check if pieces are added correctly
+    #check if "column is full" check works
+    #check if "column doesn't exist" works
+    #check if win is correctly detected vertically, horizontally, diagonally
+    #check if you can exit game at any time
+    #check if you can restart game
