f17bc40d3aacec2954dd71deaee4a05b0972237d
Juan C <juan.s.carrasquilla@gmail.com>
2023-01-22 15:33:53 -0600

    changed print_board to include join function, got rid of magic nums, moved loop and user input from board class to gameloop class

--- connect_four.py
+++ connect_four.py
@@ -1,57 +1,36 @@
 
 class ConnectFourBoard:
     class ColorEnum(Enum):
-        Y = 1
-        R = 2
+        Yellow = 1
+        Red = 2
    
     def __init__(self, c: int = 7, r: int = 6, w: int = 4) -> None:  
         self.num_cols = c #number of columns set by user, max is 26 (A-Z) min is 4
         self.num_rows = r #number of rows set by user, min is 4
         self.win_req = w #win requirement set by user
-        self.current = [[None]*self.num_cols for _ in range(self.num_rows)]
-        #self.colA, self.colB, self.colC, self.colD, self.colE, self.colF, self.colG = [], [], [], [], [], [], [] #TODO need to change to 2D matrix
+        self.current = [["_"]*self.num_cols for _ in range(self.num_rows)]
         self.columns = [[] for _ in range(c)]
-
-        #self.col_dict = {"A": [self.colA, 0], "B": [self.colB, 1], "C": [self.colC, 2], "D": [self.colD, 3], "E": [self.colE, 4], "F": [self.colF, 5], "G": [self.colG, 6]}  
-                #TODO need to fix col_dict to flex to input board size
 
     #function for printing out the current board and pieces played
     def print_board(self) -> None:
-        row_count = self.num_rows
-        for row in self.current:
-            print_row = str(row_count)
-            
-            for elem in row:
-                if elem is None:
-                    print_row += " _ "
-                else:
-                    print_row += " " + elem + " "
 
-            print(print_row)
-            row_count -= 1
+        for row_count, row in enumerate(self.current):
+            print(str(row_count) + "  " + "  ".join(row))           
 
-        print_row = " "
-        for lett in range(self.num_cols):
-            print_row += " " + chr(lett+65) + " "
-        print(print_row)
+        column_title = [chr(x + ord("A")) for x in range(self.num_cols)] #create a list of column headers starting at "A"
+        print("   " + " ".join(column_title))
 
 
     #function for checking the validity of a chosen column. Checks if col exists and is not full. 
-    def check_col(self, col: str) -> int: 
-        while True:   
-            col_i = ord(col) #using UNICODE of capital letters, ord("A") == 65     
-            
-            if col_i < 65 or col_i > 64 + self.num_cols: #max board size is 26 (A-Z)
-                print(player1 + ", that column does not exist. Please pick another column ():") #TODO change to show flexible board size
-                col = str(input()).capitalize()
-                continue
-            
-            elif len(self.columns[col_i-65]) >= self.num_rows:
-                print(player1 + ", that column is full. Please pick another column ():") # TODO change to show flexible board size
-                col = str(input()).capitalize()
-                continue
-            else:
-                return col_i-65
+    def check_col(self, col: str) -> int:   
+        col_i = ord(col) #using UNICODE of capital letters    
+        unicode_A = ord("A")  # ord("A") == 65 
+        if col_i < unicode_A or col_i > unicode_A -1 + self.num_cols: #max board size is 26 (A-Z)
+            return 1 #error code 1 = column doesn't exist
+        elif len(self.columns[col_i - unicode_A]) >= self.num_rows:
+            return 2 #error code 2 = column is full
+        else:
+            return col_i - unicode_A
 
     def __vertical_win(self, row: int, col: int, color: str) -> bool:
         if row <= self.num_rows - self.win_req: #if the current row is high enough to have a winning quantity
@@ -107,7 +86,19 @@ class GameLoop:
     def new_turn(player: str, color: str) -> None:
         print(player, "it is your turn. Enter the column you would like to add a piece to (A-G):")
         col_chosen = str(input()).capitalize() #TODO turn this query into a function, allow different input types
-        col_i = self.board.check_col(col_chosen)
+        
+        col_code = self.board.check_col(col_chosen)  #TODO turn this into separate GameLoop method
+        while col_code < 3:
+            if col_code == 1:
+                print(player1 + ", that column does not exist. Please pick another column ():") #TODO change to show flexible board size
+                col = str(input()).capitalize()
+                col_code = self.board.check_col(col_chosen)
+            else:
+                print(player1 + ", that column is full. Please pick another column ():") # TODO change to show flexible board size
+                col = str(input()).capitalize()
+                col_code = self.board.check_col(col_chosen)
+        col_i = col_code
+
         self.board.columns[col_i].append(color)
         new_row, new_col = self.board.num_rows - len(self.board.columns[col_i]), col_i 
 
